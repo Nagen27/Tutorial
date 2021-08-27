04a007a93e2999aa7bff4e353a7b058549038be2
Nagen27 <1191202423@student.mmu.edu.my>
2021-08-27 22:27:38 +0800

    Created the menu

--- T3-NagendraanVijayasegaran.cpp
+++ T3-NagendraanVijayasegaran.cpp
@@ -16,4 +16,36 @@ CarType * newNode(string carModel)
 	
 	return temp;
 }
+int main ()
+{
+	int number;
 
+	do
+	{
+		cout<<"Please choose what you want to do:"<<endl;
+		cout<<"1. Add a car."<<endl;
+		cout<<"2. Delet the next car."<<endl;
+		cout<<"3. Go to the next car."<<endl;
+		cout<<"0. Exit this program."<<endl;
+		cout<<"Enter your choice:"<<endl;
+		cin>> number;
+		
+		if(number == 1)
+		{
+			
+		}
+		else if(number == 2)
+		{
+		
+		}
+		else if(number == 3)
+		{
+	
+		}
+		else 
+		{
+		
+		}
+		
+	}while(number!=0);
+}
